efefb07a62ac79a46e796faab9d8fe9f2f6da2b6
Pratyush2710 <pratyushhg@gmail.com>
2020-07-21 19:36:14 +0530

    Project initilised

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+//  import pages
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SingleCocktail from "./pages/SingleCocktail";
+import Error from "./pages/Error";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route path="/about">
+          <About />
+        </Route>
+
+        <Route path="/cocktail/:id">
+          <SingleCocktail />
+        </Route>
+
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
