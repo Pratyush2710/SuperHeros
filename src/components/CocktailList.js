1344c6674ab6c7c708be42b764f66b4801465cc9
Pratyush2710 <pratyushhg@gmail.com>
2020-07-21 19:37:31 +0530

    Components added

---
+++ src/components/CocktailList.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function CocktailList({ cocktails }) {
+  console.log(cocktails);
+  return <h1>cocktailList component</h1>;
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../logo.svg";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <img src={logo} alt="cocktail logo" className="logo" />
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
